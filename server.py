712f8e5855b60b7872002ceedcb47235a58dfd75
Kynefa <159808071+Kynefa@users.noreply.github.com>
2025-05-25 21:13:28 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+users = {
+    "katya321": {
+        "last_name": "Соловйова",
+        "first_name": "Катерина",
+        "course": "2",
+        "group": "ІС-31"
+    }
+}
+
+@app.route('/')
+def home():
+    login = request.args.get('login')
+    if login in users:
+        user = users[login]
+        return f"""
+        <h2>Дані користувача</h2>
+        <p>Прізвище: {user['last_name']}</p>
+        <p>Ім’я: {user['first_name']}</p>
+        <p>Курс: {user['course']}</p>
+        <p>Група: {user['group']}</p>
+        """
+    else:
+        return "Користувача не знайдено або не передано login."
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
